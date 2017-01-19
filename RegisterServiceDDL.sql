6fb5f4abdeb34eacbe790c64df5c1dda07322221
Reid Phillips <rphillips@newestech.com>
2017-01-19 09:56:42 -0600

    Initial commit.

---
+++ RegisterServiceDDL.sql
@@ -0,0 +1,37 @@
+CREATE EXTENSION "uuid-ossp";
+
+CREATE TABLE product (
+  id uuid NOT NULL,
+  lookupcode character varying(32) NOT NULL DEFAULT(''),
+  count int NOT NULL DEFAULT(0),
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT product_pkey PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE INDEX ix_product_lookupcode
+  ON product
+  USING btree
+  (lower(lookupcode::text) COLLATE pg_catalog."default");
+
+INSERT INTO product VALUES (
+       uuid_generate_v4()
+     , 'lookupcode1'
+     , 100
+     , current_timestamp
+);
+
+INSERT INTO product VALUES (
+       uuid_generate_v4()
+     , 'lookupcode1'
+     , 125
+     , current_timestamp
+);
+
+INSERT INTO product VALUES (
+       uuid_generate_v4()
+     , 'lookupcode3'
+     , 150
+     , current_timestamp
+);
