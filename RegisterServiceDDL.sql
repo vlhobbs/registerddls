db3de84dd4eaf2cb18a231710b523be0998824c4
Reid Phillips <rphillips@newestech.com>
2017-03-09 15:59:09 -0600

    Sprint 2 work

--- RegisterServiceDDL.sql
+++ RegisterServiceDDL.sql
@@ -35,3 +35,23 @@ INSERT INTO product VALUES (
      , 150
      , current_timestamp
 );
+
+CREATE TABLE employee (
+  id uuid NOT NULL,
+  employeeid character varying(32) NOT NULL DEFAULT(''),
+  firstname character varying(128) NOT NULL DEFAULT(''),
+  lastname character varying(128) NOT NULL DEFAULT(''),
+  password character varying(512) NOT NULL DEFAULT(''),
+  active boolean NOT NULL DEFAULT(FALSE), 
+  classification int NOT NULL DEFAULT(0),
+  managerid uuid NOT NULL,
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT employee_pkey PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE INDEX ix_employee_employeeid
+  ON employee
+  USING hash(employeeid);
+
