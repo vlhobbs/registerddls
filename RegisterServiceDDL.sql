48b07aa9374e066461d3db5d26f049d5e286f0e8
Victoria Hobbs <vlhobbs@uark.edu>
2017-04-11 14:32:51 -0500

    Adding id to transaction1 and altering key

--- RegisterServiceDDL.sql
+++ RegisterServiceDDL.sql
@@ -17,6 +17,68 @@ CREATE INDEX ix_product_lookupcode
   USING btree
   (lower(lookupcode::text) COLLATE pg_catalog."default");
 
+CREATE TABLE employee (
+  id uuid NOT NULL,
+  employeeid character varying(32) NOT NULL DEFAULT(''),
+  firstname character varying(128) NOT NULL DEFAULT(''),
+  lastname character varying(128) NOT NULL DEFAULT(''),
+  password character varying(512) NOT NULL DEFAULT(''),
+  active boolean NOT NULL DEFAULT(FALSE), 
+  classification int NOT NULL DEFAULT(0),
+  managerid uuid NOT NULL,
+  createdon timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT employee_pkey PRIMARY KEY (id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE INDEX ix_employee_employeeid
+  ON employee
+  USING hash(employeeid);
+  
+--CREATE TYPE transaction_type AS ENUM ('sale', 'return');
+
+
+//cgmcname ~1.5 hours worked on
+
+
+CREATE TABLE transaction2 (
+        id uuid,
+	transid uuid NOT NULL,
+	itemnum int NOT NULL,
+	createdon timestamp without time zone NOT NULL DEFAULT now(),
+	productid uuid NOT NULL,
+	productnum int NOT NULL DEFAULT (1),
+	CONSTRAINT transaction2_pkey PRIMARY KEY (id),
+	CONSTRAINT productid_key FOREIGN KEY (productid)
+		REFERENCES product (id) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE NO ACTION
+			
+)	WITH (
+	OIDS=FALSE
+);
+
+
+CREATE TABLE transaction1 (
+	id uuid NOT NULL,
+	cashierid uuid NOT NULL,
+	total money NOT NULL DEFAULT 0.00,
+	transtype varchar(8) NOT NULL DEFAULT('sale'),
+	referenceid uuid NOT NULL,
+	createdon timestamp without time zone NOT NULL DEFAULT(now()),
+	CONSTRAINT transaction1_pkey PRIMARY KEY (recordid),
+ 	CONSTRAINT transaction1_employee_fkey FOREIGN KEY (cashierid)
+		REFERENCES employee (id) MATCH SIMPLE 
+		ON UPDATE NO ACTION ON DELETE NO ACTION, 
+	CONSTRAINT transaction1_tran2_fkey FOREIGN KEY (referenceid)
+		REFERENCES transaction2 (transid) MATCH SIMPLE
+		ON UPDATE NO ACTION ON DELETE NO ACTION, 
+	CONSTRAINT type_check CHECK (transtype IN ('sale', 'return'))
+)	WITH (
+	OIDS=FALSE
+);
+
+
 
 INSERT INTO product VALUES (
        uuid_generate_v4()
@@ -44,62 +106,3 @@ INSERT INTO product VALUES (
      , 400
      , current_timestamp
 );
-
-CREATE TABLE employee (
-  id uuid NOT NULL,
-  employeeid character varying(32) NOT NULL DEFAULT(''),
-  firstname character varying(128) NOT NULL DEFAULT(''),
-  lastname character varying(128) NOT NULL DEFAULT(''),
-  password character varying(512) NOT NULL DEFAULT(''),
-  active boolean NOT NULL DEFAULT(FALSE), 
-  classification int NOT NULL DEFAULT(0),
-  managerid uuid NOT NULL,
-  createdon timestamp without time zone NOT NULL DEFAULT now(),
-  CONSTRAINT employee_pkey PRIMARY KEY (id)
-) WITH (
-  OIDS=FALSE
-);
-
-CREATE INDEX ix_employee_employeeid
-  ON employee
-  USING hash(employeeid);
-  
---CREATE TYPE transaction_type AS ENUM ('sale', 'return');
-
-
-//cgmcname ~1.5 hours worked on
-CREATE TABLE transaction1 (
-	recordid uuid NOT NULL,
-	cashierid uuid NOT NULL,
-	total money NOT NULL DEFAULT 0.00,
-	transtype varchar(8) NOT NULL DEFAULT('sale'),
-	referenceid uuid NOT NULL,
-	createdon timestamp without time zone NOT NULL DEFAULT(now()),
-	CONSTRAINT transaction1_pkey PRIMARY KEY (recordid),
- 	CONSTRAINT transaction1_employee_fkey FOREIGN KEY (cashierid)
-		REFERENCES employee (id) MATCH SIMPLE 
-		ON UPDATE NO ACTION ON DELETE NO ACTION, 
-	CONSTRAINT transaction1_tran2_fkey FOREIGN KEY (referenceid)
-		REFERENCES transaction2 (transid) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE NO ACTION, 
-	CONSTRAINT type_check CHECK (transtype IN ('sale', 'return'))
-)	WITH (
-	OIDS=FALSE
-);
-
-
-CREATE TABLE transaction2 (
-	transid uuid NOT NULL,
-	itemnum int NOT NULL,
-	createdon timestamp without time zone NOT NULL DEFAULT now(),
-	productid uuid NOT NULL,
-	productnum int NOT NULL DEFAULT (1),
-	CONSTRAINT transaction2_pkey PRIMARY KEY (transid, itemnum),
-	CONSTRAINT productid_key FOREIGN KEY (productid)
-		REFERENCES product (id) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE NO ACTION
-			
-)	WITH (
-	OIDS=FALSE
-);
-
